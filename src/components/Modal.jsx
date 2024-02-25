adc2e707d09083e8cc688328588f0f2efffeb342
prasetion <prasetion@gmail.com>
2024-02-25 20:31:29 +0700

    dialog modal animation

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,14 +1,26 @@
-import { createPortal } from 'react-dom';
+import { createPortal } from "react-dom";
+import { motion } from "framer-motion";
 
 export default function Modal({ title, children, onClose }) {
+  const initState = {
+    opacity: 0,
+    y: 30,
+  };
+
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
-      <dialog open className="modal">
+      <motion.dialog
+        initial={initState}
+        animate={{ opacity: 1, y: 0 }}
+        exit={initState}
+        open
+        className="modal"
+      >
         <h2>{title}</h2>
         {children}
-      </dialog>
+      </motion.dialog>
     </>,
-    document.getElementById('modal')
+    document.getElementById("modal")
   );
 }
